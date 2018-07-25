24a6db5db30ff9784f7b16354535b3d5f63f6939
Demo User <demouser@MacBook-Pro.home>
2018-07-24 22:41:07 -0400

    js,css& html for todo  commented them out becasue messed with original

---
+++ static/css/homepage.js
@@ -0,0 +1,24 @@
+// var editSmall = document.querySelector("small");
+// var Textinput = document.querySelector("input")
+// var span = document.querySelectorAll("span");
+// var input = document.querySelector("input[type= 'text']")
+//
+// editsmall.addEventListener("click",function(){
+//  Textinput.style.display= "block";
+// });
+// input.onkeypress= (function(evt){
+// 	if(evt.which == 13){
+// 		var Todo = document.createElement("LI")
+// 		var TodoText = document.createText(this.value);
+// 		Todo.appendChild("TodoText");
+// 		document.getElementById("text-holder").appendChild(Todo);
+// 	}
+//
+// })
+//
+// for (var i = 0; i < span.length;i++){
+// 	span[i].addEventListener("click",function(){
+// 		   this.parentElement.style.display="none";
+// 	});
+//
+// }
